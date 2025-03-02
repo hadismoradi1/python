147d1de7f91a7926453500330be3438d502fc5bc
hadismoradi1 <hadismoradi13858@gmail.com>
2025-03-02 23:22:52 +0330

    Add files via upload

---
+++ username.py
@@ -0,0 +1,22 @@
+#count=0
+#while count<5:
+list1=[]
+a=int(input('enter your username:'))
+b=int(input('enter your username:'))
+c=int(input('enter your username:'))
+#if list1!=-1:
+for h in list1:
+    list1.append(a)
+    list1.append(b)
+    list1.append(c)
+                    
+
+#else:
+    #print('end')
+           
+    #print(list1)    
+   # count+=1
+print(list1)
+list1.sort()
+print(list1)
+
